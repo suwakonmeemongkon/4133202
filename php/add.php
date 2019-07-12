56f40d8e15a3bf187c3b56cff9e4a7ce5fbacc8c
suwakonmaeemongkon <suwakon.13@gmail.com>
2019-07-12 15:41:56 +0700

    init project

---
+++ php/add.php
@@ -0,0 +1,19 @@
+<?php
+echo $_POST['user'];
+
+echo "Hello World !!!";
+
+$name = "Anukool";
+echo "Mr." . $name;
+echo "<br>";
+
+echo sum(5,3);
+
+function sum($A,$B){
+    return $A + $B ;
+
+}
+?>
+
+//short term, use one line only.
+<?='My name is Nu'?>
